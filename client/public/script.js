8cacf44b86fb6abf2817cc7377ab8af60a44b167
ricsimarta <ricsimarta@gmail.com>
2024-07-31 19:25:45 +0200

    half-done last task

--- client/public/script.js
+++ client/public/script.js
@@ -35,12 +35,41 @@ const convertMsToTime = (ms) => {
   seconds -= minutes * 60;
   let hours = Math.floor(minutes / 60);
   minutes -= hours * 60;
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().length < 2 ? `0${seconds}` : seconds}`
+  /* 
 
-  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}` */
 }
 
 const loadEvent = function () {
-  console.log(products[0]);
+  console.log(products);
+
+  let albumsGroupedByVendorName = {};
+
+  /* products.forEach(album => 
+    albumsGroupedByVendorName[album.vendor.name] ? 
+    albumsGroupedByVendorName[album.vendor.name].push(album) : 
+    albumsGroupedByVendorName[album.vendor.name] = [album]
+  ) */
+  // console.log(albumsGroupedByVendorName)
+
+  albumsGroupedByVendorName = products.reduce((acc, curr) => {
+    acc[curr.vendor.name] ? acc[curr.vendor.name].push(curr) : acc[curr.vendor.name] = [curr]
+    return acc
+  }, {})
+
+  /* const vendorNames = Object.keys(albumsGroupedByVendorName);
+  vendorNames.forEach(vendorName => {
+    albumsGroupedByVendorName[vendorName].forEach(album => {
+      console.log(vendorName, album)
+    })
+  }) */
+
+  for (const key in albumsGroupedByVendorName) {
+    albumsGroupedByVendorName[key].forEach(album => {
+      console.log(key, album)
+    })
+  }
 
   const rootElement = document.querySelector("#root");
   const albumsHtml = products
